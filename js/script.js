7adf5aa8629707955b5351e98d7b4b444260fd0c
Team-CODEit <152992011+Team-CODEit@users.noreply.github.com>
2023-12-09 15:26:44 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", () => {
-    document.querySelector("header").classList.toggle("scrolled", window.scrollY > window.innerHeight - 70)
-})
-
-console.log(window.innerHeight)
